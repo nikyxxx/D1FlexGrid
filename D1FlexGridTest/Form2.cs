800f66f0e64e0b799b4943251879e269a472cb09
nikyxxx <works_nbb@163.com>
2012-03-31 16:35:17 +0800

    edit !!!haha

---
+++ D1FlexGridTest/Form2.cs
@@ -0,0 +1,25 @@
+﻿/******************************************************************************
+ *  作者：       Administrator
+ *  创建时间：   3/31/2012 4:34:47 PM
+ *
+ *
+ ******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace D1FlexGridTest
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
